29448912918c611a329fee1cabd30519ef7260a5
maehara-keisuke <mk.rosjp@gmail.com>
2025-07-05 18:48:53 +0900

    modify transport code for wiznet

--- platform_code/arduino/wiznet_ethernet/micro_ros_transport.h
+++ platform_code/arduino/wiznet_ethernet/micro_ros_transport.h
@@ -1,15 +1,22 @@
-#include <NativeEthernet.h>
+#include <Ethernet.h>
 
 struct micro_ros_agent_locator {
 	IPAddress address;
 	int port;
 };
 
-static inline void set_microros_native_ethernet_transports(byte mac[], IPAddress client_ip, IPAddress agent_ip, uint16_t agent_port){
+static inline void set_microros_wiznet_ethernet_transports(byte mac[], IPAddress client_ip, IPAddress agent_ip, uint16_t agent_port, uint8_t slave_select_pin, bool use_dhcp, IPAddress& client_dynamic_ip){
 
 	static struct micro_ros_agent_locator locator;
 
-   	Ethernet.begin(mac, client_ip);
+	Ethernet.init(slave_select_pin);
+
+	if (use_dhcp == true){
+		Ethernet.begin(mac);
+		client_dynamic_ip = Ethernet.localIP();
+	} else {
+		Ethernet.begin(mac, client_ip);
+	}
 	delay(1000);
 
 	locator.address = agent_ip;
