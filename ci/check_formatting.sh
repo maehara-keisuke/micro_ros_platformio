1e602e005a752577254a2150f040f733e360a124
maehara-keisuke <mk.rosjp@gmail.com>
2025-07-20 20:40:35 +0900

    fix

--- ci/check_formatting.sh
+++ ci/check_formatting.sh
@@ -2,6 +2,7 @@
 
 find . -regex '.*\.\(cpp\|hpp\|c\|h\)' -exec clang-format -dry-run -Werror -style=file:".clang-format" -i {} \;
 formatting_violated=$?
-if [$formatting_violated=1]; then
+echo $formatting_violated
+if [ $formatting_violated -eq 1]; then
 	exit 1
 fi
