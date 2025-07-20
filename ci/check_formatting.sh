13567d92f3ae80ffd090c7af28b876041dfb9867
maehara-keisuke <mk.rosjp@gmail.com>
2025-07-20 20:37:22 +0900

    fix

--- ci/check_formatting.sh
+++ ci/check_formatting.sh
@@ -2,6 +2,6 @@
 
 find . -regex '.*\.\(cpp\|hpp\|c\|h\)' -exec clang-format -dry-run -Werror -style=file:".clang-format" -i {} \;
 formatting_violated=$?
-if [formatting_violated=1]; then
+if [$formatting_violated=1]; then
 	exit 1
 fi
